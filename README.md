6597dca518b16bacc750e7be24520723ab7ec709
Purnima Lamba <purnima.lamba@kelltontech.com>
2018-07-17 18:36:23 +0530

    get slots functionality added

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # react-native-slot-creator
+Creates time slots for iOS and Android React-Native apps , this package is in heavy development phase.
+
+## PLANNED FEATURES
+
+- [x] Get time slots between given start time and end time
+- [x] Get slots in required format (12hrs and 24hrs both)
+- [x] Remove the inbetween breaktime slots if required.
+- [ ] DateObject manupulation
+- [ ] Dynamic UI for slots
+
+
+### Installation
+
+1. ```npm install react-native-slot-creator --save```
+
+
+
+### Example
+
+
+```jsx
+import React from 'react';
+import { View, Image } from 'react-native';
+import { slotCreator } from "react-native-slot-creator";
+
+
+export default class App extends Component {
+  componentWillMount(){
+    // Default format
+    slotCreator.createSlot(startTime, endTime, slotDuration, breakStartTime, breakEndTime, is12HoursFormat)
+
+    // To get the Array of slots between given time interval with breakTime and 12 Hrs format
+    let requiredArray = slotCreator.createSlot("08:00","10:00","20","09:00","09:30",true)
+    console.warn("requiredArray",requiredArray)
+
+
+    // To get the Array of slots between given time interval without breakTime 
+    let requiredArray = slotCreator.createSlot("08:00","10:00","20")
+    console.warn("requiredArray",requiredArray)
+    
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+      </View>
+    );
+  }
+}
+
+
+```
+
+| Parameter  | Default  | Format | Description | Required |
+| :------------ |:---------------:| :---------------:| :-----|
+| startTime | null | `24 Hrs` | Start time of your time interval  | `Yes` |
+| endTime | null | `24 Hrs` | End time of your time interval   | `Yes` |
+| slotDuration | null | `Mins` | Duration of slot you require | `Yes` |
+| breakStartTime | null | `24 Hrs` | Break start time | `optional` |
+| breakEndTime | null | `24 Hrs` |  Break start time | `optional` |
+| is12HoursFormat | false | `bool` | If you require the result in 12 Hrs format set this as true | `optional` |
+
---
+++ slotCreator.js
@@ -0,0 +1,117 @@
+/**
+ *
+ * slotCreator Module
+ * @author Purnima Lamba
+ *
+ */
+
+import { Platform } from 'react-native';
+
+function createSlot(startTime, endTime, slotDuration, breakStartTime, breakEndTime, is12HoursFormat) {
+    var startTimeInminutes = null;
+    var endTimeInminutes = null;
+    var breakStartTimeInminutes = null;
+    var breakEndTimeInminutes = null;
+    let slotArray = []
+    let initialSlotArray = []
+    let initialBreaktimeArray = []
+    try {
+
+        let [startTimeHour, startTimeMinute] = startTime.split(":")
+        let [endTimeHour, endTimeMinute] = endTime.split(":")
+        startTimeInminutes = (parseInt(startTimeHour) * 60) + parseInt(startTimeMinute);
+        endTimeInminutes = (parseInt(endTimeHour) * 60) + parseInt(endTimeMinute);
+       
+        if (breakStartTime && breakEndTime){
+            let [breakStartTimeHour, breakStartTimeMinute] = breakStartTime.split(":")
+            let [breakEndTimeHour, breakEndTimeMinute] = breakEndTime.split(":")
+            breakStartTimeInminutes = (parseInt(breakStartTimeHour) * 60) + parseInt(breakStartTimeMinute);
+            breakEndTimeInminutes = (parseInt(breakEndTimeHour) * 60) + parseInt(breakEndTimeMinute);
+        }
+        
+    } catch (error) {
+    }
+  
+    if (slotDuration > 0) {
+        if (startTimeInminutes != 0 && endTimeInminutes != 0) {
+            let diffOfTime = null
+            if (endTimeInminutes > startTimeInminutes) {
+                diffOfTime = endTimeInminutes - startTimeInminutes
+            } else {
+                diffOfTime = startTimeInminutes - endTimeInminutes
+            }
+            if (diffOfTime > 0) {
+                let numberOfSlots = diffOfTime / slotDuration
+                for (var slots = 0; slots < numberOfSlots; slots++) {
+                    let timeSlot = convertMinsToHrsMins(startTimeInminutes + (slots * slotDuration))
+                    initialSlotArray.push(timeSlot)
+                }
+            }
+        }
+        if (breakStartTime && breakEndTime) {
+        if (breakStartTimeInminutes != 0 && breakEndTimeInminutes != 0) {
+            let diffOfBreakTime = null
+            if (breakEndTimeInminutes > breakStartTimeInminutes) {
+                diffOfBreakTime = breakEndTimeInminutes - breakStartTimeInminutes
+            } else {
+                diffOfBreakTime = breakStartTimeInminutes - breakEndTimeInminutes
+            }
+            
+            if (diffOfBreakTime > 0) {
+                let numberOfSlots = diffOfBreakTime / slotDuration
+                for (let slots = 0; slots < numberOfSlots ; slots++) {
+                    let timeSlot = convertMinsToHrsMins(breakStartTimeInminutes + (slots * slotDuration))
+                    initialBreaktimeArray.push(timeSlot)
+                }
+            }
+        }
+    }
+        if (initialBreaktimeArray.length > 0 && initialSlotArray.length > 0) {
+            let lengthCount = initialBreaktimeArray.length - 1
+            for (let count = 0; count < initialSlotArray.length; count++) {
+                for (let counter = 0; counter < initialBreaktimeArray.length; counter++) {
+                    if (initialSlotArray[count] >= initialBreaktimeArray[0] && initialSlotArray[count] <= initialBreaktimeArray[lengthCount]) {
+                        initialSlotArray.splice(count,1)
+                    }
+                }
+            }
+            slotArray = initialSlotArray
+
+        } else {
+            slotArray = initialSlotArray
+        }
+        if (is12HoursFormat && slotArray.length>0){
+            to12HrsFormat(slotArray)
+        }else{
+            return slotArray;
+        }
+    } else {
+        console.warn("Slot duration not defined")
+    }
+}
+var slotCreator = {
+    createSlot: createSlot,
+}
+function convertMinsToHrsMins(mins) {
+    let h = Math.floor(mins / 60);
+    let m = mins % 60;
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    return `${h}:${m}`;
+}
+function to12HrsFormat(slotArray) {
+    let slotArrayFormatted=[]
+    let timeSlot =null
+    for(let count = 0 ; count < slotArray.length ; count++){
+       let[h,m] = slotArray[count].split(":")
+       if(h >= 12){
+           timeSlot = h % 12 + ":" + m + " P.M"
+       }else{
+           timeSlot = h + ":" + m + " A.M"
+       }
+        slotArrayFormatted.push(timeSlot)
+    }
+}
+module.exports = {
+    slotCreator
+};
